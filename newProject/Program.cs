cf98f105e3ddeee14b1c1d14d7e61b1ab781d3e2
Marleny Garrido Lazaro <74108713+mArlettGL@users.noreply.github.com>
2021-02-19 23:11:38 -0600

    Vamos bien

---
+++ newProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace newProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t¡Hola Mundo!"+"\n\nSabías que 2 + 2 = Pez");
+        }
+    }
+}
